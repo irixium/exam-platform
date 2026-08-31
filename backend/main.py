f3f39aa64b2836e4199993e0d86681fd11a70aa7
Anshuman <prox19dec@gmail.com>
2026-08-31 14:29:09 +0000

    ftr: add route protection dependency

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, Depends
 from starlette.middleware.sessions import SessionMiddleware
 from routers import auth
 from app_secrets import session_secret_key
@@ -7,6 +7,27 @@ from database import get_connection
 init_db()
 
 app = FastAPI()
+
+async def get_current_user(request: Request):
+    user_id = request.session.get("user_id")
+    if not user_id:
+        raise HTTPException(
+            status_code=401,
+            detail="Not authenticated",
+        )
+
+    with get_connection() as conn:
+        cursor = conn.execute("SELECT username FROM users WHERE id = ?", (user_id,))
+        row = cursor.fetchone()
+        if row:
+            return row[0]
+
+    
+    raise HTTPException(
+        status_code=401,
+        detail="Invalid session",
+    )
+
 
 app.add_middleware(SessionMiddleware, secret_key=session_secret_key)
 
@@ -17,12 +38,5 @@ def health_check():
     return {"status": "ok"}
 
 @app.get("/")
-def root(request: Request):
-    user_id = request.session.get("user_id")
-    if user_id:
-        with get_connection() as conn:
-            cursor = conn.execute("SELECT username FROM users WHERE id = ?", (user_id,))
-            row = cursor.fetchone()
-            if row:
-                return {"message": f"Welcome {row[0]} to the Exam Catalog API"}
-    return HTTPException(status_code=401, detail="Unauthorized. Please log in to access this resource.")
+def root(user: str = Depends(get_current_user)):
+    return {"message": f"Welcome {user} to the Exam Catalog API"}
--- backend/routers/exam_catalog.py
+++ backend/routers/exam_catalog.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Request, UploadFile, File, HTTPException
+from fastapi import APIRouter, Request, UploadFile, File, HTTPException, Depends
 from schemas.exam_catalog import CatalogItem, Question
 from services.exam_catalog import get_catalog_items, upload
+from main import get_current_user
 import csv
 from io import StringIO
 router = APIRouter()
 
 @router.post("/upload-csv")
-def upload_exam(data: CatalogItem, csv_file: UploadFile = File(...)):
+def upload_exam(data: CatalogItem, csv_file: UploadFile = File(...), user: str = Depends(get_current_user)):
     return upload(data, csv_file)
 
 
@@ -14,5 +15,5 @@ def upload_exam(data: CatalogItem, csv_file: UploadFile = File(...)):
 
 
 @router.get("/exam-list", response_model=list[CatalogItem])
-def get_catalog():
+def get_catalog(user: str = Depends(get_current_user)):
     return get_catalog_items()
