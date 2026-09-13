d6be48e01053cc8774598e40bda1ab9a8ff918ca
Anshuman <prox19dec@gmail.com>
2026-09-13 10:25:34 +0000

    fix: bug in root API

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException, Depends
 from starlette.middleware.sessions import SessionMiddleware
 from routers import auth, exam_catalog, exam_attempt
+from schemas.auth import AuthenticatedUser
 from services.auth import get_current_user
 from app_secrets import session_secret_key
 from database import init_db
@@ -19,6 +20,5 @@ def health_check():
     return {"status": "ok"}
 
 @app.get("/")
-def root(user_info: tuple = Depends(get_current_user)):
-    username, is_admin = user_info
-    return {"message": f"Welcome {username} to the Exam Catalog API", "is_admin": is_admin, "username": username}
+def root(user_info: AuthenticatedUser = Depends(get_current_user)):
+    return {"message": f"Welcome {user_info.username} to the Exam Catalog API", "is_admin": user_info.is_admin, "username": user_info.username}
