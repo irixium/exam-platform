e3603e5cdc278d687463bb86c6795635a4da0975
Anshuman <prox19dec@gmail.com>
2026-09-19 17:56:55 +0000

    fix setup script

--- scripts/setup_app.sh
+++ scripts/setup_app.sh
@@ -1,7 +1,12 @@
-cd ../backend
+#!/bin/bash
+
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+ROOT_DIR="$SCRIPT_DIR/.."
+
+cd "$ROOT_DIR/backend"
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.tx
 
-cd ../frontend
+cd "$ROOT_DIR/frontend"
 npm install
