73525c3040185c880cc5e96dffdba21459193007
Anshuman <prox19dec@gmail.com>
2026-09-19 18:00:51 +0000

    fix typo in setup script

--- scripts/setup_app.sh
+++ scripts/setup_app.sh
@@ -6,7 +6,7 @@ ROOT_DIR="$SCRIPT_DIR/.."
 cd "$ROOT_DIR/backend"
 python3 -m venv .venv
 source .venv/bin/activate
-pip install -r requirements.tx
+pip install -r requirements.txt
 
 cd "$ROOT_DIR/frontend"
 npm install
